9156bf25dd4c1fb7591f08e1d539a2e215126d60
jim Bramnick <jbramnick412@gmail.com>
2022-02-05 21:15:20 -0500

    Initial commit

---
+++ drgsync.py
@@ -0,0 +1,51 @@
+#DRG save syncer
+#Author: James Bramnick
+import os
+import shutil
+
+xboxFolder=r"C:\Users\Jim\AppData\Local\Packages\CoffeeStainStudios.DeepRockGalactic_496a1srhmar9w\SystemAppData\wgs\000901F908B63662_882901006F2042808DB0569531F199CB\984D039726D54AD88F440C0F3556E2B1"
+steamFolder=r"C:\Program Files (x86)\Steam\steamapps\common\Deep Rock Galactic\FSD\Saved\SaveGames"
+
+def sync():
+    steamFile=""
+    xboxFile=""
+    xboxFileName=""
+    steamFileName=""
+    xtime = None
+    stime = None
+    xbackNum = 0
+    sbackNum = 0
+    for file in os.listdir(xboxFolder):
+            
+        if "." not in file:
+                xboxFile=os.path.join(xboxFolder,file)
+                xtime = os.path.getmtime(xboxFile)
+                xboxFileName = file
+        if file.endswith(".bak"):
+                temp = int(file.split("__")[1])
+                if temp >= xbackNum:
+                                xbackNum = temp+1
+    for file in os.listdir(steamFolder):
+        if file.endswith('_Player.sav'):
+                steamFile=os.path.join(steamFolder,file)
+                stime = os.path.getmtime(steamFile)
+                steamFileName = file
+        if file.endswith(".bak"):
+                temp = int(file.split("__")[1])
+                if temp >= sbackNum:
+                                sbackNum = temp+1
+    if stime > xtime:
+            shutil.copy(xboxFile,os.path.join(xboxFolder,xboxFileName+"__"+str(xbackNum)+"__.bak"))
+            shutil.copy(steamFile,xboxFile)
+            print ("Steam file copied to xbox")
+    elif xtime > stime:
+            shutil.copy(steamFile,os.path.join(steamFolder,steamFileName+"__"+str(sbackNum)+"__.bak"))
+            shutil.copy(xboxFile,steamFile)
+            print ("Xbox file copied to steam")
+    else:
+        print ("Something is wonky or your files have the exact same time maybe play some DRG. Rock and Stone brother")
+def main():
+    sync()
+    input("Press any key to close application")
+if __name__ == "__main__":
+        main()
